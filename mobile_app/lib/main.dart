cc9865b3920cc263ccd0dcaca8505016f7234b02
samo7a <aelshetany@knights.ucf.edu>
2021-08-17 02:59:07 -0400

    Added the logo and a spinner to the splash screen

--- mobile_app/lib/main.dart
+++ mobile_app/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import './util/Size.dart';
+import './screens/SplashScreen.dart';
 
 void main() {
   runApp(MyApp());
@@ -24,27 +24,6 @@ class MyApp extends StatelessWidget {
       routes: {
         '/': (context) => SplashScreen(),
       },
-    );
-  }
-}
-
-class SplashScreen extends StatelessWidget {
-  const SplashScreen({Key? key}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-          colors: [
-            Color(0xff001845),
-            Color(0xffffffff),
-            Color(0xff001845),
-          ],
-        ),
-      ),
-      child: Center(),
     );
   }
 }
---
+++ mobile_app/lib/screens/SplashScreen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import '../util/Size.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    Size size = Size(Context: context);
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Color(0xff001845),
+            Color(0xffffffff),
+            Color(0xff001845),
+          ],
+        ),
+      ),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image(
+              image: AssetImage('images/splashIcon.png'),
+              width: size.BLOCK_WIDTH * 50,
+              height: size.BLOCK_HEIGHT * 50,
+            ),
+            SpinKitFadingCircle(
+              color: Color(0xff002855),
+              size: size.BLOCK_WIDTH * 15,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
