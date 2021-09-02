1eeba67069b413d6422c3832906d72a255f64568
samo7a <aelshetany@knights.ucf.edu>
2021-09-02 17:22:20 -0400

    saving status

--- website/src/pages/Landing/LandingPage.jsx
+++ website/src/pages/Landing/LandingPage.jsx
@@ -3,10 +3,13 @@ import "./LandingPage.css";
 import Navbar from "../../components/Navbar/Navbar";
 import Footer from "../../components/Footer/Footer";
 import vid from "../../assets/promo.mp4";
+import icon3 from "../../assets/icon3.jpg";
 import { signOut } from "../../auth/signout";
+import { useHistory } from "react-router-dom";
 
 const LandingPage = () => {
   let isLoggedIn = false;
+  const history = useHistory();
   useEffect(() => {
     signOut();
   }, []);
@@ -19,19 +22,10 @@ const LandingPage = () => {
             <article className="post">
               <h1>Share a Ride, Save The Planet!</h1>
               <p className="para">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-                porta nunc at nisi laoreet, eget dapibus velit semper. Vivamus
-                id hendrerit mi. Cras odio urna, blandit a odio vel, dapibus
-                varius odio. Donec elit lacus, mollis ac sem non, luctus
-                sollicitudin velit. Cras tincidunt tellus lectus, a consequat
-                nulla venenatis sed. Sed sed quam a erat lobortis sollicitudin
-                porttitor ullamcorper ex. Fusce accumsan nunc ut justo interdum
-                tristique in at nisl. Morbi mattis pulvinar rhoncus. Mauris quam
-                risus, pulvinar et urna et, convallis accumsan nisl. Praesent
-                nec lectus vel velit lobortis lobortis. Etiam sollicitudin,
-                sapien eget porta euismod, elit massa egestas est, id blandit mi
-                ligula eu neque. Nulla interdum nibh nisi, non mollis mi luctus
-                ac.
+                Join us now and help us to raise the awarness of carpooling as a
+                transportation option. Our Application is designed to cut down
+                the number of cars on the road. This will reduce the carbon
+                emissions into the air resulting in a cleaner environment.
               </p>
             </article>
           </div>
@@ -44,6 +38,32 @@ const LandingPage = () => {
             <a href="https://www.vecteezy.com/video/2905810-cheerful-woman-spread-arms-on-car-rooftop-under-bright-sky-at-mountain-with-nature-background-during-road-trip-on-vacation">
               https://www.vecteezy.com
             </a>
+          </div>
+        </div>
+        <div className="wrapper">
+          <div className="left">
+            <article className="post">
+              <h1>Find a ride in a convenient, fast, and affordable way!</h1>
+              <h2> How Karpool works?</h2>
+              <p className="para">
+                We will look for rides along your route to fill your empty seats
+                in the car, so you and the rider can save money, and contribute
+                in the green movement. It is a win-win-win situation! <br /> So
+                What are you waiting for? Signup now!
+              </p>
+            </article>
+            <button
+              id="secondaryButton"
+              onClick={() => {
+                history.push("/register");
+              }}
+            >
+              Sign Up
+            </button>
+          </div>
+
+          <div className="right">
+            <img src={icon3} alt="how-karpool-works" width="100%" />
           </div>
         </div>
         <Footer />
